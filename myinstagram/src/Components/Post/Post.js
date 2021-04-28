f3fe1f4fe87eabeaf1b2a7a3d9769facd5065285
naka <ryousen.naka@microworld.co.jp>
2021-04-28 17:57:04 +0900

    main画面

--- myinstagram/src/Components/Post/Post.js
+++ myinstagram/src/Components/Post/Post.js
@@ -1,7 +1,10 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Post.css";
-import postimage from "../../images/post.jpg"
+import postimage from "../../images/post.jpg";
+import love from "../../images/love.svg";
+import comment from "../../images/comment.svg";
+import share from "../../images/share.svg";
 
 const Post = () => {
   return (
@@ -14,14 +17,27 @@ const Post = () => {
 
       {/* Image */}
       <div>
-        <img src={postimage} />
+        <img src={postimage} width="615px" />
       </div>
 
       {/* Analytics */}
-      <div></div>
+      <div>
+        <div style={{ marginLeft: "10px" }}>
+          <img src="{love}" className="post_reactimage" />
+          <img src="{comment}" className="post_reactimage" />
+          <img src="{share}" className="post_reactimage" />
+        </div>
+        <div style={{ boldWeight: "bold", marginLeft: "20px" }}>7749 likes</div>
+      </div>
 
       {/* Comment Section */}
-      <div></div>
+      <div>
+        <div className="post__comment">Hello comment 1</div>
+        <div className="post__comment">Hello comment 1</div>
+        <div className="post__comment">Hello comment 1</div>
+        <div className="post__comment">Hello comment 1</div>
+        <input text="text" className="post__commentbox" placeholder="add a comment..." />
+      </div>
     </div>
   );
 };
